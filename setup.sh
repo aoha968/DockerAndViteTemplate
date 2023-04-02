49a377baf13875d80a47c05b63bbd52e86cd16b6
aoha <aoha.kurobane168@gmail.com>
2023-04-02 15:18:54 +0900

    First Commit

---
+++ setup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# cloneしてきたGit情報はいらないので削除する
+#rm -rf .git
+# 新しくGit管理を始める
+#git init
+
+# 書き換えできるように権限をつける
+chmod a+rw Dockerfile
+chmod a+rw docker-compose.yml
+
+# サービス名を引数で受け取る
+sed -i '' 's/REPLACE/'"$1"'/g' docker-compose.yml
+
+# Docker Image作成
+docker-compose build
+
+# Vite導入
+docker-compose run $1 yarn create vite vite-project --template vanilla
+
+# コメント行を削除する
+sed -i -e 's/^#//' Dockerfile
+sed -i -e 's/^#//' docker-compose.yml
+
+# Project nameで指定したディレクトリに移動
+cd vite-project
+
+# node_modulesを入手
+docker run --rm -v ${PWD}:/app -w /app node:18-alpine yarn install
+
+# Project nameで指定したディレクトリに移動
+mv ../docker-compose.yml ./docker-compose.yml
+mv ../Dockerfile ./Dockerfile
+
+# 不要なファイルを消す
+rm ../docker-compose.yml-e
+rm ../Dockerfile-e
+
+# ビルド
+docker-compose build --no-cache
